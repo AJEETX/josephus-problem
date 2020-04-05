17221f724269373e44f452742d7acbce7126791e
azy <ajeet.kumar-tiwari@agilent.com>
2020-04-05 11:57:36 +1000

    ceated logic

---
+++ Josephus.Library.Test/Josephus.Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using xUnit;
+namespace Josephus.Library.Test
+{
+    class JosephusTest
+    {
+        [Fact]
+        public void GetResult()
+        {
+            //given
+            int childrenCount = 10,stepCount=3;
+
+            //when
+            var actualResult= Josephus.
+
+            //then
+        }
+    }
+}
---
+++ Josephus.Problem/Josephus.Library/ResultData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Josephus.Library
+{
+    class ResultData
+    {
+        public IEnumerable<int> RemovedChildrenSequence { get; set; }
+        public int WinningChild { get; set; }
+    }
+}
